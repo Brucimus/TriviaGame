5a8299f6105e6ff2d55b83b509af4bdbce32651e
Brucimus <Brucimus@gmail.com>
2018-06-24 23:39:55 -0700

    added notation

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+//variables
+var questions = [
+    Q1 = {
+        questions: "",
+        possibleAnswers: [
+
+        ],
+        correctAnswer: ""
+    },
+    Q2 = {
+        questions: "",
+        possibleAnswers: [
+
+        ],
+        correctAnswer: ""
+    },
+]
+
+var answeredCorrectly = "",
+
+//randomize questions
+
+//set timer function
+
+    //display question using random number
+
+    //loop through display corresponding answer possibilities
+
+    //on click compare to correct answer index
+        
+        //if correct stop timer and display answered correctly
+            
+            //correct answer ++
+
+        //if incorrect stop timer and display answered incorrectly and display correct answer
